ab8b9431a4e402e4e40020ec30977693f0c11081
cheerfyt <tate_fan@163.com>
2020-04-17 10:31:33 +0800

    feat(ignore): #PLM-1172 GlobalConfigSetting

---
+++ src/main/java/io/jenkins/plugins/worktile/WTGlobalConfiguration.java
@@ -0,0 +1,89 @@
+package io.jenkins.plugins.worktile;
+
+import javax.annotation.Nonnull;
+import hudson.Extension;
+import hudson.Util;
+import hudson.util.FormValidation;
+import jenkins.model.GlobalConfiguration;
+import jenkins.model.Jenkins;
+import net.sf.json.JSONObject;
+
+import org.apache.commons.lang3.StringUtils;
+import org.kohsuke.stapler.DataBoundSetter;
+import org.kohsuke.stapler.QueryParameter;
+import org.kohsuke.stapler.StaplerRequest;
+
+@Extension
+public class WTGlobalConfiguration extends GlobalConfiguration {
+    public static final String PRODUCT_ENDPOINT = "https://open.worktile.com";
+
+    private String endpoint;
+    private String clientId;
+    private String secretKey;
+
+    @DataBoundSetter
+    public void setEndpoint(String endpoint) {
+        this.endpoint = Util.fixEmptyAndTrim(endpoint);
+    }
+
+    @DataBoundSetter
+    public void setSecretKey(String secretKey) {
+        this.secretKey = Util.fixEmptyAndTrim(secretKey);
+    }
+
+    @DataBoundSetter
+    public void setClientId(String clientId) {
+        this.clientId = Util.fixEmptyAndTrim(clientId);
+    }
+
+    public String getSecretKey() {
+        return secretKey;
+    }
+
+    public String getClientId() {
+        return clientId;
+    }
+
+    public String getEndpoint() {
+        return endpoint;
+    }
+
+    public WTGlobalConfiguration() {
+        load();
+    }
+
+    @Override
+    public boolean configure(StaplerRequest req, JSONObject formatData) throws FormException {
+        String endpoint = formatData.getString("endpoint");
+        if (StringUtils.isBlank(endpoint)) {
+            endpoint = WTGlobalConfiguration.PRODUCT_ENDPOINT;
+        }
+        setEndpoint(endpoint);
+        setClientId(formatData.getString("clientId"));
+        setSecretKey(formatData.getString("secretKey"));
+        save();
+        return true;
+    }
+
+    public FormValidation doCheckEndpoint(@QueryParameter(value = "endpoint", fixEmpty = true) String endpoint) {
+        if (StringUtils.isNotBlank(endpoint) && (!endpoint.startsWith("http") || !endpoint.startsWith("https"))) {
+            return FormValidation.error("endpoint wrong");
+        }
+        return FormValidation.ok();
+    }
+
+    public FormValidation doCheckClientId(@QueryParameter(value = "clientId", fixEmpty = true) String clientId) {
+        return StringUtils.isNotBlank(clientId) ? FormValidation.ok()
+                : FormValidation.error("client id can not be empty");
+    }
+
+    public FormValidation doCheckSecretKey(@QueryParameter(value = "clientId", fixEmpty = true) String secretkey) {
+        return StringUtils.isNotBlank(secretkey) ? FormValidation.ok()
+                : FormValidation.error("secret key can not be empty");
+    }
+
+    @Nonnull
+    public static WTGlobalConfiguration get() {
+        return (WTGlobalConfiguration) Jenkins.get().getDescriptorOrDie(WTGlobalConfiguration.class);
+    }
+}
