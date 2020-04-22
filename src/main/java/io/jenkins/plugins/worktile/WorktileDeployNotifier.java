5a5fb4f648329e38128ea41448f62fde2c0e93c4
cheerfyt <tate_fan@163.com>
2020-04-22 10:05:14 +0800

    feat(release) #PLM-1197 add UI

--- src/main/java/io/jenkins/plugins/worktile/WorktileDeployNotifier.java
+++ src/main/java/io/jenkins/plugins/worktile/WorktileDeployNotifier.java
@@ -5,10 +5,13 @@ import hudson.model.AbstractProject;
 import hudson.tasks.BuildStepDescriptor;
 import hudson.tasks.Notifier;
 import hudson.tasks.Publisher;
+import hudson.util.FormValidation;
+import hudson.util.ListBoxModel;
 import net.sf.json.JSONObject;
 import org.jetbrains.annotations.NotNull;
 import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.DataBoundSetter;
+import org.kohsuke.stapler.QueryParameter;
 import org.kohsuke.stapler.StaplerRequest;
 
 import java.util.ArrayList;
@@ -18,9 +21,33 @@ public class WorktileDeployNotifier extends Notifier {
 
     private List<String> environments;
 
+    private String releaseName;
+
+    private String releaseUrl;
+
     @DataBoundConstructor
-    public WorktileDeployNotifier(List<String> environments) {
-       setEnvironments(environments);
+    public WorktileDeployNotifier(final List<String> environments, final String releaseName, final String releaseUrl) {
+        setEnvironments(environments);
+        setReleaseName(releaseName);
+        setReleaseUrl(releaseUrl);
+    }
+
+    public String getReleaseName() {
+        return releaseName;
+    }
+
+    @DataBoundSetter
+    public void setReleaseName(final String releaseName) {
+        this.releaseName = releaseName;
+    }
+
+    public String getReleaseUrl() {
+        return releaseUrl;
+    }
+
+    @DataBoundSetter
+    public void setReleaseUrl(final String releaseUrl) {
+        this.releaseUrl = releaseUrl;
     }
 
     public List<String> getEnvironments() {
@@ -28,7 +55,7 @@ public class WorktileDeployNotifier extends Notifier {
     }
 
     @DataBoundSetter
-    public void setEnvironments(List<String> environments) {
+    public void setEnvironments(final List<String> environments) {
         this.environments = new ArrayList<>(environments);
     }
 
@@ -38,8 +65,9 @@ public class WorktileDeployNotifier extends Notifier {
             super(WorktileDeployNotifier.class);
         }
 
+        @SuppressWarnings("rawtypes")
         @Override
-        public boolean isApplicable(Class<? extends AbstractProject> item) {
+        public boolean isApplicable(@NotNull final Class<? extends AbstractProject> item) {
             return true;
         }
 
@@ -49,9 +77,22 @@ public class WorktileDeployNotifier extends Notifier {
         }
 
         @Override
-        public WorktileDeployNotifier newInstance(StaplerRequest request, @NotNull JSONObject formData) {
+        public WorktileDeployNotifier newInstance(final StaplerRequest request, @NotNull final JSONObject formData) {
             assert request != null;
             return request.bindJSON(WorktileDeployNotifier.class, formData);
+        }
+
+        public FormValidation doCheckReleaseName(
+                @QueryParameter(value = "releaseName", fixEmpty = true) String releaseName) {
+            if (WorktileUtils.isBlank(releaseName))
+                return FormValidation.error("release name can not be empty");
+            return FormValidation.ok();
+        }
+
+        public ListBoxModel doFillEnvironmentsItems() {
+            final ListBoxModel items = new ListBoxModel();
+            items.add("hello", "world");
+            return items;
         }
     }
 }
