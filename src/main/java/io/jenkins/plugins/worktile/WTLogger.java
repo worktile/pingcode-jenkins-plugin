dda9f568722c3a3da2e8f50e5f544af955f17d0d
cheerfyt <tate_fan@163.com>
2020-05-11 10:16:07 +0800

    feat(refactor) add apiConnection

---
+++ src/main/java/io/jenkins/plugins/worktile/WTLogger.java
@@ -0,0 +1,21 @@
+package io.jenkins.plugins.worktile;
+
+import java.io.Serializable;
+
+import hudson.model.TaskListener;
+
+public class WTLogger implements Serializable {
+    private TaskListener listener;
+
+    public WTLogger(TaskListener listener) {
+        this.listener = listener;
+    }
+
+    public void info(String message) {
+        this.listener.getLogger().println("Worktile - [INFO] " + message);
+    }
+
+    public void error(String message) {
+        this.listener.getLogger().println("Worktile - [ERROR] " + message);
+    }
+}
