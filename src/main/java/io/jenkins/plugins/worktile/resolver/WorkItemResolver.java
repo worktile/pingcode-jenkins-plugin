b11702298aef1bc732b2b6762bbf6833d9fc846d
cheerfyt <tate_fan@163.com>
2020-05-24 22:00:13 +0800

    feat(resolver): #PLM-1365 get commit in tag

--- src/main/java/io/jenkins/plugins/worktile/resolver/WorkItemResolver.java
+++ src/main/java/io/jenkins/plugins/worktile/resolver/WorkItemResolver.java
@@ -3,17 +3,24 @@ package io.jenkins.plugins.worktile.resolver;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.logging.Logger;
 import java.util.regex.Pattern;
 
+import org.bouncycastle.jcajce.provider.asymmetric.dsa.DSASigner.stdDSA;
 import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.LogCommand;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.internal.storage.file.FileRepository;
 import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.Ref;
 import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.RevWalk;
 import org.jenkinsci.plugins.workflow.job.WorkflowRun;
 
 import hudson.EnvVars;
@@ -59,7 +66,13 @@ public class WorkItemResolver {
         try {
             fromScm();
         } catch (final Exception e) {
-            wtLogger.info("Extract work items error " + e.getMessage());
+            wtLogger.info("Extract work items error from message body " + e.getMessage());
+        }
+
+        try {
+            fromTag();
+        } catch (final Exception e) {
+            wtLogger.info("Extract work items error from tag " + e.getMessage());
         }
         final List<String> matches = WTHelper.matches(pattern, new ArrayList<>(collection), false, false);
         return WTHelper.formatWorkItems(matches);
@@ -120,6 +133,19 @@ public class WorkItemResolver {
         collection.addAll(messages);
     }
 
+    public void fromTag() throws IOException, InterruptedException, GitAPIException {
+        if (run == null || workspace == null) {
+            return;
+        }
+        final boolean isGit = workspace.child(VCSFolder).exists();
+        if (!isGit) {
+            wtLogger.info("unsupported vcs, current git only");
+        }
+        final FilePath gitStoreDir = workspace.child(VCSFolder);
+        List<String> messages = gitStoreDir.act(new GitTagsCallback(listener));
+        collection.addAll(messages);
+    }
+
     @SuppressWarnings("rawtypes")
     private RunWithSCM<?, ?> toSCMRun() {
         RunWithSCM runWithScm = null;
@@ -168,6 +194,83 @@ public class WorkItemResolver {
                 git.close();
                 return messages;
             }
+        }
+    }
+
+    private static final class GitTagsCallback extends MasterToSlaveFileCallable<List<String>> {
+        private static final long serialVersionUID = -247109644349075954L;
+
+        private TaskListener listener;
+
+        public GitTagsCallback(TaskListener listener) {
+            this.listener = listener;
+        }
+
+        private void sort(List<Ref> tags, RevWalk walk) {
+            Collections.sort(tags, new Comparator<Ref>() {
+                public int compare(Ref t1, Ref t2) {
+                    java.util.Date d1 = null;
+                    java.util.Date d2 = null;
+                    try {
+                        d1 = walk.parseCommit(t1.getObjectId()).getCommitterIdent().getWhen();
+                        d2 = walk.parseCommit(t2.getObjectId()).getCommitterIdent().getWhen();
+                        return d2.compareTo(d1);
+                    } catch (Exception e) {
+                        System.out.println(e);
+                    }
+                    return 0;
+                }
+            });
+        }
+
+        @Override
+        public List<String> invoke(final File file, final VirtualChannel virtualChannel)
+                throws IOException, InterruptedException {
+            List<String> messages = new ArrayList<>();
+            WTLogger wtLogger = new WTLogger(listener);
+            if (!file.exists() || !file.isDirectory()) {
+                return messages;
+            }
+            try (FileRepository fileRepository = new FileRepository(file.getAbsolutePath())) {
+                List<Ref> tags = null;
+                Git git = new Git(fileRepository);
+                try {
+                    tags = git.tagList().call();
+                    this.sort(tags, new RevWalk(fileRepository));
+                    if (tags != null && !tags.isEmpty()) {
+                        Ref tag0 = tags.get(0);
+                        Ref peeledRef0 = fileRepository.getRefDatabase().peel(tag0);
+                        wtLogger.info("tags.get(0) = " + tag0.getName());
+                        ObjectId utilId = peeledRef0.getPeeledObjectId() != null ? peeledRef0.getPeeledObjectId()
+                                : tag0.getObjectId();
+
+                        Ref tag1 = tags.get(1);
+                        ObjectId startId = null;
+                        if (tag1 != null) {
+                            wtLogger.info("tags.get(1) = " + tag1.getName());
+                            Ref peeledRef1 = fileRepository.getRefDatabase().peel(tag1);
+                            startId = peeledRef1.getPeeledObjectId() != null ? peeledRef1.getPeeledObjectId()
+                                    : tag1.getObjectId();
+                        }
+
+                        LogCommand log = git.log().addRange(startId, utilId);
+                        Iterable<RevCommit> logs = log.call();
+                        for (RevCommit commit : logs) {
+                            if (commit != null) {
+                                String message = commit.getFullMessage();
+                                logger.info("message in tags " + message);
+                                if (message != null) {
+                                    messages.add(message);
+                                }
+                            }
+                        }
+                    }
+                } catch (Exception exception) {
+                    wtLogger.error("get commit message in tag error");
+                }
+                git.close();
+            }
+            return messages;
         }
     }
 }
