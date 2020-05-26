9810d13a9b8292653914793cb70d0b63b2340dfc
cheerfyt <tate_fan@163.com>
2020-05-26 21:42:29 +0800

    fix(resultOverview): fix resolve empty result overview from log

--- src/main/java/io/jenkins/plugins/worktile/MemoryTokenStore.java
+++ src/main/java/io/jenkins/plugins/worktile/MemoryTokenStore.java
@@ -7,23 +7,25 @@ import java.util.Map;
 
 public class MemoryTokenStore {
 
-  public static final Map<String, WTTokenEntity> store = new HashMap<>();
+    protected static final Map<String, WTTokenEntity> store = new HashMap<>();
 
-  public static boolean put(String clientId, String clientSecret, WTTokenEntity entity) {
-    try {
-      String key = WTHelper.md5(clientId + clientSecret);
-      return store.put(key, entity) != null;
-    } catch (Exception e) {
-      return false;
+    public static boolean put(String clientId, String clientSecret, WTTokenEntity entity) {
+        try {
+            String key = WTHelper.md5(clientId + clientSecret);
+            return store.put(key, entity) != null;
+        }
+        catch(Exception e) {
+            return false;
+        }
     }
-  }
 
-  public static WTTokenEntity get(String clientId, String clientSecret) {
-    try {
-      String key = WTHelper.md5(clientId + clientSecret);
-      return store.get(key);
-    } catch (Exception exception) {
-      return null;
+    public static WTTokenEntity get(String clientId, String clientSecret) {
+        try {
+            String key = WTHelper.md5(clientId + clientSecret);
+            return store.get(key);
+        }
+        catch(Exception exception) {
+            return null;
+        }
     }
-  }
 }
